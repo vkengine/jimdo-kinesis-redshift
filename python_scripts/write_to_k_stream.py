078209d52020120a52a7d1db482e1e46fb88337e
vanketesh-k <vanketesh@tribes.ai>
2021-09-06 10:36:32 +0530

    semi final commit - will release one more revision 🏁

--- python_scripts/write_to_k_stream.py
+++ python_scripts/write_to_k_stream.py
@@ -11,15 +11,14 @@ k_client = boto3.client('firehose', region_name='eu-central-1')
 
 def lambda_handler(event=None, context=None):
     for i in range(10):
-
-        put_to_stream(i+1)
+        put_to_stream(i + 1)
 
         # wait for 1 second
         time.sleep(1)
 
 
 def put_to_stream(count):
-    payload_user_utm = {
+    payload_user_event = {
         "request_id": "fc417c2b-fe7a-4737-afe2-bec49c0ebd6f",
         "request_timestamp": "2020-06-01 00:00:24.000000",
         "cookie_id": "a09c4ca3-d142-4312-ba78-029c3355b8ef",
@@ -33,7 +32,7 @@ def put_to_stream(count):
         "referrer": "https://register.jimdo.com/es/product"
     }
 
-    payload_user_event = {
+    payload_user_utm = {
         "request_id": "cbae8544-ab7a-46a5-9253-a5c8f6612115",
         "source": "adwords",
         "medium": "cpc",
